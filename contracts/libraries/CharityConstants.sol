4f0e7f96b1715e3786edd65ca87d3877dc9a47d9
mino <sh00283ml@SH00283MLdeMacBook-Pro.local>
2022-08-31 21:20:31 +0800

    signatureVerification code

--- contracts/libraries/CharityConstants.sol
+++ contracts/libraries/CharityConstants.sol
@@ -1,2 +1,59 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
+
+uint256 constant OneWord = 0x20;
+uint256 constant MaxUint8 = 0xff;
+uint256 constant MaxUint120 = 0xffffffffffffffffffffffffffffff;
+
+
+// Signature-related
+bytes32 constant EIP2098_allButHighestBitMask = (
+    0x7fffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff
+);
+bytes32 constant ECDSA_twentySeventhAndTwentyEighthBytesSet = (
+    0x0000000000000000000000000000000000000000000000000000000101000000
+);
+uint256 constant ECDSA_MaxLength = 65;
+uint256 constant ECDSA_signature_s_offset = 0x40;
+uint256 constant ECDSA_signature_v_offset = 0x60;
+
+bytes32 constant EIP1271_isValidSignature_selector = (
+    0x1626ba7e00000000000000000000000000000000000000000000000000000000
+);
+uint256 constant EIP1271_isValidSignature_signatureHead_negativeOffset = 0x20;
+uint256 constant EIP1271_isValidSignature_digest_negativeOffset = 0x40;
+uint256 constant EIP1271_isValidSignature_selector_negativeOffset = 0x44;
+uint256 constant EIP1271_isValidSignature_calldata_baseLength = 0x64;
+
+uint256 constant EIP1271_isValidSignature_signature_head_offset = 0x40;
+
+
+// Declare constant for errors related to signature verification.
+uint256 constant Ecrecover_precompile = 1;
+uint256 constant Ecrecover_args_size = 0x80;
+uint256 constant Signature_lower_v = 27;
+
+// error BadSignatureV(uint8) @ SignatureVerificationErrors.sol
+uint256 constant BadSignatureV_error_signature = (
+    0x1f003d0a00000000000000000000000000000000000000000000000000000000
+);
+uint256 constant BadSignatureV_error_offset = 0x04;
+uint256 constant BadSignatureV_error_length = 0x24;
+
+// error InvalidSigner() @ SignatureVerificationErrors.sol
+uint256 constant InvalidSigner_error_signature = (
+    0x815e1d6400000000000000000000000000000000000000000000000000000000
+);
+uint256 constant InvalidSigner_error_length = 0x04;
+
+// error InvalidSignature() @ SignatureVerificationErrors.sol
+uint256 constant InvalidSignature_error_signature = (
+    0x8baa579f00000000000000000000000000000000000000000000000000000000
+);
+uint256 constant InvalidSignature_error_length = 0x04;
+
+// error BadContractSignature() @ SignatureVerificationErrors.sol
+uint256 constant BadContractSignature_error_signature = (
+    0x4f7fb80d00000000000000000000000000000000000000000000000000000000
+);
+uint256 constant BadContractSignature_error_length = 0x04;
