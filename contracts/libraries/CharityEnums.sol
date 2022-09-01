468bf31111ce140e0ddb2e618a4f4abd096ccc9a
mino <sh00283ml@SH00283MLdeMacBook-Pro.local>
2022-09-01 17:37:42 +0800

    charity structs

--- contracts/libraries/CharityEnums.sol
+++ contracts/libraries/CharityEnums.sol
@@ -1,2 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
+
+
+enum ProjectState {
+    //0: Project 上链的初始状态，等待审核，审核中状态
+    CENSORING, 
+    //1: Project 募捐中
+    
+}
--- contracts/libraries/CharityStructs.sol
+++ contracts/libraries/CharityStructs.sol
@@ -1,3 +1,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
+import {
+    ProjectState,
+    CensorState
+} from "./CharityEnums.sol";
+
+
+struct Censor {
+    uint256 censorId; //生成censor的Id
+    address censorAddress; //censor的受益与转账地址
+    uint  depositBalance; //质押的余额
+    uint  processingNum; //当前正在处理未结项审查的数据
+    CensorState state;
+
+}
